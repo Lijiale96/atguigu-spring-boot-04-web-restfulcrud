1fa2f601f0899a37d6b23c755b07fcc7168011c1
Lijiale96 <ljl512@163.com>
2020-05-18 22:28:21 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springboot/config/MyServerConfig.java
@@ -0,0 +1,54 @@
+package com.atguigu.springboot.config;
+
+import com.atguigu.springboot.filter.MyFilter;
+import com.atguigu.springboot.listener.MyListener;
+import com.atguigu.springboot.servlet.MyServlet;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletRegistration;
+import java.util.Arrays;
+
+@Configuration
+public class MyServerConfig {
+    //注册三大组件
+    @Bean
+    public ServletRegistrationBean  myServlet(){
+        ServletRegistrationBean registrationBean=new ServletRegistrationBean<>(new MyServlet(),"/myServlet");
+        registrationBean.setLoadOnStartup(1);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean myFilter(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new MyFilter());
+        registrationBean.setUrlPatterns(Arrays.asList("/hello","/myServlet"));
+        return registrationBean;
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean  myListener(){
+        ServletListenerRegistrationBean<MyListener> registrationBean=new ServletListenerRegistrationBean<MyListener>(new MyListener());
+        return registrationBean;
+    }
+
+
+    //配置嵌入式的Servlet容器
+    @Bean
+    public WebServerFactoryCustomizer webServerFactoryCustomizer(){
+        return new WebServerFactoryCustomizer() {
+            @Override
+            public void customize(WebServerFactory factory) {
+//                factory.
+            }
+            //定制嵌入式的Servlet容器相关的规则
+        };
+    }
+}
---
+++ src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -0,0 +1,42 @@
+package com.atguigu.springboot.controller;
+
+import com.atguigu.springboot.exception.UserNotExistException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+//    @RequestMapping( {"/","index.html"})
+//    public String index(){
+//        return "index";
+//    }
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String helloWorld(@RequestParam("user") String user) {
+        if (user.equals("aaa")){
+            throw new UserNotExistException();
+        }
+        return "hello world";
+    }
+
+    @RequestMapping("success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("张三","李四","王五"));
+        map.put("user", new Object(){
+           public  String id="111";
+           public String getName(){
+               return "张三";
+           }
+        });
+//        thymeleaf 默认路径为 classpath:/templates
+        return "success";
+    }
+}
